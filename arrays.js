eb369ff3e0ab080d35e037ae32179c6cb8de9d4c
Esteban Flores <ebflores88@gmail.com>
2020-02-18 04:48:53 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -57,3 +57,5 @@ mutate the original array.*/
 removeElementFromBeginningOfArray = function(array){
   return array.slice(1);
 }
+
+/*Define a function in arrays.js called destructivelyRemoveElementFromEndOfArray that takes an array as its only argument and removes the last element. Your function should return the entire array, and it should mutate the array.*/
