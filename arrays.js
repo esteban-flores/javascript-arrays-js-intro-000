1daf3dd2b4c7374e4166a38fc00cf108bf1cc80b
Esteban Flores <ebflores88@gmail.com>
2020-02-18 04:10:45 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,7 +1,7 @@
 var chocolateBars = ["snickers","hundred grand","kitkat","skittles"];
 
 var array = [];
-addElementToBeginningOfArray = function(array,element){
+addElementToBeginningOfArray = function(array, element){
   var newArray = [];
   newArray = [element, ...newArray];
   return newArray;
