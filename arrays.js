2ab1b7cbe53691e779143d9498e7de3cc9b08b53
Esteban Flores <ebflores88@gmail.com>
2020-02-18 04:02:27 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -7,7 +7,7 @@ addElementToBeginningOfArray = function([],element){
 //Both functions take two parameters, an array and an element
 //to add to the beginning of the array, and both functions
 //should add the element to the beginning of the array and then
-// return the whole array.
+//return the whole array.
 
 /*
 destructivelyAddElementToBeginningOfArray = function(){
