82b88aae2a42f180c67acaab1cfec3fc20d7f1f3
Esteban Flores <ebflores88@gmail.com>
2020-02-18 05:14:57 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -70,9 +70,9 @@ destructivelyRemoveElementFromEndOfArray = function(array){
 }
 //destructivelyRemoveElementFromEndOfArray(chocolateBars);
 
-/*Define a function called removeElementFromEndOfArray that takes 
-an array as its only argument and removes the last element. Your 
-function should return the array without the last element, and it 
+/*Define a function called removeElementFromEndOfArray that takes
+an array as its only argument and removes the last element. Your
+function should return the array without the last element, and it
 should not mutate the original array.*/
 removeElementFromEndOfArray = function(array){
 
